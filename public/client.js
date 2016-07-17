5d64d44bd041bb3c96683ae38bad64065bb755e0
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-16 18:29:30 -0600

    Add listener for individual vote and styling

--- public/client.js
+++ public/client.js
@@ -1,1 +1,41 @@
+// const $ = require('jquery');
 var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users at the moment:   ' + count;
+});
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+   socket.send('voteCast', this.innerText);
+  });
+}
+
+var voteCount = document.getElementById('vote-count');
+
+socket.on('voteCount', function(votes) {
+  for (var letter in votes) {
+    console.log(votes[letter]);
+    var voteTarget = document.getElementById('letter-' + letter);
+	  voteTarget.innerText = 'Votes for ' + letter + ' : ' + votes[letter];
+    // var voteNumber = document.getElementsByClassName('badge');
+    // voteNumber.innerText = votes[letter];
+  }
+
+  var yourVote = document.getElementById('your-vote');
+
+  socket.on('yourVote', function (vote) {
+  	yourVote.innerText = "You voted for " + vote;
+  });
+
+});
